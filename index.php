b9661e4218ac9603ce468651816c43312268457c
Paytonnpp <paykelper@gmail.com>
2024-01-09 19:42:36 -0600

    Tied shoelaces

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $name="MyName";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hellow World</title>
+    <title>Hello World</title>
 </head>
 <body>
     <h1>Hello Payton</h1>
